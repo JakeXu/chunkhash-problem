643ecc4f5ff45d823a4205948a298fddf19d5371
Kevin Renskers <info@mixedcase.nl>
2015-07-30 23:28:22 +0000

    Add babel-loader, oops

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,6 @@ const angular = require('angular');
 const _ = require('underscore');
 
 if (angular.isString('') && _.isArray([])) {
-  const myVar = '2';
+  const myVar = '1';
   console.log(myVar);
 }
