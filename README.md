4a2aa4af2e1769bef692d32e20bd3fb298351593
Kevin Renskers <info@mixedcase.nl>
2015-07-30 23:24:03 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chunkhash-problem
-Showing a problem where vendor chunkhashes are not always the same even when the contents don't change
+Showing a problem where vendor chunkhashes are not always the same even when the contents don't change.
+
+Just edit `src/entry.js`, change the value of `myVar` and run `npm run build`.
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+var webpack = require('webpack');
+var ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+
+var out = path.join(__dirname, 'build');
+
+// clear output
+rimraf.sync(out);
+fs.mkdirSync(out);
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    main: ['./entry.js'],
+    vendor: ['angular', 'underscore']
+  },
+
+  output: {
+    path: out,
+    filename: '[name].[chunkhash].js'
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity
+    }),
+
+    new ChunkManifestPlugin({
+      filename: 'chunk-manifest.json',
+      manifestVariable: 'webpackManifest'
+    }),
+
+    new webpack.optimize.DedupePlugin(),
+
+    new webpack.optimize.OccurenceOrderPlugin(),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    })
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
+    ]
+  }
+};
